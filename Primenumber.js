e49d0592e42bfa5351a5cdd59e0e77774c672384
alokt1994 <alokt1994@gmail.com>
2024-08-27 19:36:36 +0530

    add primenumber program

--- Primenumber.js
+++ Primenumber.js
@@ -5,12 +5,13 @@ if (num == 1) {
 } else if (num < 1) {
   console.log('Number is not prime');
 } else {
-  for (var i = 2; i <= num; i++) {
+  for (var i = 2; i<num; i++) {
     if (num % i == 0) {
-      console.log(`This is not a prime number${num}`);
+      var res = `This is not a prime number ${num}`;
       break;
     } else {
-      console.log(`Number is prime ${num}`);
+      var res = `Number is prime ${num}`;
     }
   }
+  console.log(res);
 }
